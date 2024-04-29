e49dddde6bde9bf752044118bc1ce31d0da384e1
lucasens1 <lucasensini1@gmail.com>
2024-04-29 18:01:34 +0200

    minor fix

--- js/script.js
+++ js/script.js
@@ -4,23 +4,25 @@ createApp({
     data(){
         return{
             emailArray : [],
+            loadFlag : true,
         };
     },
     methods : {
         getRandomEmail : function(){
-            console.log("ok click")
-
+            /* console.log("ok click") */
             for(let i = 0; i < 10 ; i++){
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((curEmail) => {
-                        console.log(curEmail);
+                        /* console.log(curEmail); */
                         const email = curEmail.data.response;
-                        console.log(email);
+                        /* console.log(email); */
                         this.emailArray.push(email);
                     });
-            }
-            
+                //Reset . . .
+                this.emailArray = [];
+                this.loadFlag = false;
+            }            
         },
     },
 }).mount("#app");
